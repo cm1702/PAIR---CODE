83cd36f729dd334586a427bd6ff267c65a92780d
cm1702 <144173161+cm1702@users.noreply.github.com>
2024-04-23 22:37:52 +0530

    Add files via upload

---
+++ server/src/db/connec.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+
+const DB = process.env.DATABASE
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(()=>{
+    console.log('Connection Successful');
+}).catch((err)=>console.log(err));
